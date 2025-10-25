7b968d680cca233665f80997f330709f2a94a7a1
deepkhurana <36783904+deepkhurana1999@users.noreply.github.com>
2025-10-25 13:17:54 -0400

    feat: update docker-compose and scripts for improved Ollama container management

--- kill.sh
+++ kill.sh
@@ -3,3 +3,5 @@
 docker stop lekhanai-backend
 docker rm lekhanai-backend
 docker image remove lekhanai-backend
+docker stop lekhanai-ollama
+docker rm lekhanai-ollama
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker compose up
+docker compose up -d && docker exec -it lekhanai-ollama ollama pull qwen3:1.7b
