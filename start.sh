d3f637ce8ce5b892f5963b000e3d70492ea8494f
deepkhurana <36783904+deepkhurana1999@users.noreply.github.com>
2025-11-01 08:59:14 -0400

    chore: update LLM_MODEL version in template.env and adjust start.sh script for new model pull

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker compose up -d && docker exec -it lekhanai-ollama ollama pull qwen3:1.7b
+docker compose up -d
+sleep 10
+docker exec -it lekhanai-ollama ollama pull qwen3:8b-q4_K_M
